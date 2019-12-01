9c567f0687a4983422fe9d3161622a5cd201ff39
Akilesh Kannan <akileshkannan@gmail.com>
2019-12-01 15:55:55 +0530

    idkfml

--- ALU.v
+++ ALU.v
@@ -1,27 +1,7 @@
-module ALU (output reg[7:0] reg_out, output reg[3:0] SREG, input[3:0] function_select_lines, input[7:0] operandA, input[7:0] operandB);
-    /*
-    wire adderSubtractor_enable;
-    wire increment_enable;
-    wire comparator_enable;
-    wire or_enable;
-    wire and_enable;
-    wire shifter_enable;
-    wire not_enable;
+module ALU (output reg[7:0] reg_out, output reg[3:0] SREG, input[3:0] function_select_lines, input[7:0] operandA, input[7:0] operandB, input clk);
+    wire signed[7:0] operandA, operandB;
 
-    assign adderSubtractor_enable = (function_select_lines[3:1] == 3'b000)?1:0;
-    assign incrementer_enable = (function_select_lines == 4'b0010)?1:0;
-    assign comparator_enable = (function_select_lines == 4'b0011)?1:0;
-    assign or_enable = (function_select_lines == 4'b0100)?1:0;
-    assign and_enable = (function_select_lines == 4'b0101)?1:0;
-    assign shifter_enable = (function_select_lines[3:1] == 3'b011)?1:0;
-    assign not_enable = (function_select_lines == 4'b1000)?1:0;
-    assign xor_enable = (function_select_lines == 4'b1001)?1:0;
-
-
-    adderSubtractor8Bit adder_subtractor (SREG, reg_out, operandA, operandB, function_select_lines[0], adderSubtractor_enable);
-    adderSubtractor8Bit incrementer (SREG, operandA, operandA, 8'b1, 1'b0, incrementer_enable);
-    */
-    always @ (function_select_lines, operandA, operandB) begin
+    always @ (posedge clk, operandA, operandB, function_select_lines) begin
         case(function_select_lines)
             4'b0000: begin
                          SREG <= 4'b0000;
@@ -52,7 +32,7 @@ module ALU (output reg[7:0] reg_out, output reg[3:0] SREG, input[3:0] function_s
                          SREG[3] <= (~reg_out[7]&operandA[7]&~operandB[7]) | (reg_out[7]&~operandA[7]&operandB[7]);
                      end
             4'b0101: begin
-                         SREG[0] <= (operandA <= operandB)?1:0;
+                         SREG[0] <= ({operandA[7], operandA} <= {operandB[7], operandB})?1:0;
                          SREG[1] <= 1'b0;
                          SREG[2] <= 1'b0;
                          SREG[3] <= 1'b0;
