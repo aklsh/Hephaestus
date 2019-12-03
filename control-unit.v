5c206ab8b871c1e9ade02b7668cc4ec7e95fbe33
Akilesh Kannan <akileshkannan@gmail.com>
2019-12-03 20:19:06 +0530

    deleted behavioral

---
+++ control-unit.v
@@ -0,0 +1,6 @@
+module CU (input[32:0] instruction);
+    wire[7:0] operandA, operandB;
+    wire[4:0] opcode;
+
+    
+endmodule
--- structural-model/clock.v
+++
@@ -1,6 +0,0 @@
-module clock #(parameter tickRate = 1) (output reg clk);
-    initial begin
-        clk = 1'b0;
-        forever #tickRate clk = ~clk;
-    end
-endmodule
